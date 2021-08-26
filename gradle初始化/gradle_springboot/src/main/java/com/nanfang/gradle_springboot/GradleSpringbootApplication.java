8aa0fb6cf539607f341772b2372fc99f14fd105b
zqf <1967252394@qq.com>
2021-08-26 20:00:20 +0800

    初始化项目

---
+++ gradle初始化/gradle_springboot/src/main/java/com/nanfang/gradle_springboot/GradleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nanfang.gradle_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSpringbootApplication.class, args);
+    }
+
+}
---
+++ gradle初始化/gradle_springboot/src/test/java/com/nanfang/gradle_springboot/GradleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanfang.gradle_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
