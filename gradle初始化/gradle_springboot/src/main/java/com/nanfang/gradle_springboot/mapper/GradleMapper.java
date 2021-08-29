9ef0f1c41c66625b007548d7206825827819bdf2
zqf <1967252394@qq.com>
2021-08-29 10:07:07 +0800

    增加了生产、开发环境和注解

--- gradle初始化/gradle_springboot/src/main/java/com/nanfang/gradle_springboot/mapper/GradleMapper.java
+++ gradle初始化/gradle_springboot/src/main/java/com/nanfang/gradle_springboot/mapper/GradleMapper.java
@@ -3,5 +3,9 @@ package com.nanfang.gradle_springboot.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nanfang.gradle_springboot.pojo.User;
 
+import java.util.List;
+
 public interface GradleMapper extends BaseMapper<User> {
+
+    public List<User>findAll();
 }
